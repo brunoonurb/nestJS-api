5b1ee47dfe8434f5cab4a9ab884f9160847eac38
brunoonurb <bruno.1301817@fapi-pinhais.edu.br>
2021-07-10 19:06:56 -0300

    add projeto nestJs com Sequelize

---
+++ src/app.module.ts
@@ -0,0 +1,27 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { SequelizeModule } from '@nestjs/sequelize';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { ProdutosController } from './produtos.controller';
+import { Produto } from './produtos.models';
+import { ProdutosServices } from './produtos.service';
+
+@Module({
+  imports: [ConfigModule.forRoot(),
+    SequelizeModule.forRoot({
+      dialect: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: process.env.USUARIO_BANCO_DADOS,
+      password: process.env.SENHA_BANCO_DADOS,
+      database: 'livraria',
+      autoLoadModels: true,
+      synchronize: true,
+    }),
+    SequelizeModule.forFeature([Produto])
+  ],
+  controllers: [AppController, ProdutosController],
+  providers: [AppService, ProdutosServices],
+})
+export class AppModule {}
